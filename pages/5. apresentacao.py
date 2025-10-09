d9b4f2b6818f9712d714710e82cb405c17946a69
marcosguimaraes176 <marcos.guimaraes176@gmail.com>
2025-10-09 16:51:10 +0000

    final sub etapa 1

--- pages/5. apresentacao.py
+++ pages/5. apresentacao.py
@@ -1,5 +1,8 @@
 import streamlit as st
 import time
+import pandas as pd
+import numpy as np
+import openpyxl
 st.title("Programa Música na Rede")
 #st.header("Escolas, Estudantes, Projetos")
 st.markdown(
@@ -14,21 +17,22 @@ col1, col2=st.columns([0.50,0.50])
 
 with st.sidebar:
     st.markdown("[Coluna1](#projetos)")
-with col1:
-    #st.subheader("Bandas nas Escolas")
-    st.markdown(
-    """
-    <h1 style='font-size:20px; color:#808000;'>
-        Projeto Bandas nas Escolas
-    </h1>
-    """,
-    unsafe_allow_html=True
-)
-    st.image("fotos/bandas.jpeg", caption="bandas")
-    with st.container(border=True,horizontal=True):
-        st.text("Quantidade de Escolas Bandas:")
-        st.text("Quantidade de Estudantes Bandas:")
-    
+with st.container (border=1):
+    with col1:
+        #st.subheader("Bandas nas Escolas")
+        st.markdown(
+        """
+        <h1 style='font-size:20px; color:#808000;'>
+            Projeto Bandas nas Escolas
+        </h1>
+        """,
+        unsafe_allow_html=True
+    )
+        st.image("fotos/bandas.jpeg", caption="bandas")
+        with st.container(border=True,horizontal=True):
+            st.text("Quantidade de Escolas Bandas:")
+            st.text("Quantidade de Estudantes Bandas:")
+        
 
 with col2:
     #st.subheader("Orquestra de Violões nas Escolas")
